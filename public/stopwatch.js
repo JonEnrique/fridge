a8ad37974063ffce3328adcbfb15f6506aa51b15
JonEnrique <100391192+JonEnrique@users.noreply.github.com>
2022-06-05 10:17:20 +1000

    PLEASEWORK

--- public/stopwatch.js
+++ public/stopwatch.js
@@ -1,44 +1,49 @@
-var seconds = 00;
-var tens = 00;
-var appendTens = document.getElementById("tens");
-var appendSeconds = document.getElementById("seconds");
-var buttonStart = document.getElementById("button-start");
-var buttonStop = document.getElementById("button-stop");
-var buttonReset = document.getElementById("button-reset");
-var interval;
+window.onload = function(){
+    var seconds = 00;
+    var tens = 00;
+    var OutputSeconds = document.getElementById("second");
+    var OutputTens = document.getElementById("tens");
+    var buttonStart = document.getElementById("button-start");
+    var buttonStop = document.getElementById("button-stop");
+    var buttonReset = document.getElementById("button-reset");
+    var Interval;
 
-function startTimer() {
-    tens++;
+    buttonStart.addEventListener('click', () => {
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);  // millisecond 10 = 0.01 second
+    });
 
-    if (tens < 9) {
-        appendTens.innerHTML = "0" + tens;
-    }
-    if (tens > 9) {
-        appendTens.innerHTML = tens;
-    }
-    if (tens > 99) {
-        seconds++;
-        appendSeconds.innerHTML = "0" + seconds;
-        tens = 0;
-        appendTens.innerHTML = "0" + 0;
-    }
-    if (seconds > 9) {
-        appendSeconds.innerHTML = seconds;
+    buttonStop.addEventListener('click', () => {
+        clearInterval(Interval);
+    });
+
+    buttonReset.addEventListener('click', () => {
+        clearInterval(Interval);
+        tens = "00";
+        seconds = "00";
+        OutputSeconds.innerHTML = seconds;
+        OutputTens.innerHTML = tens;
+    });
+
+    function startTimer(){
+        tens++;
+        if(tens <= 9){
+            OutputTens.innerHTML = "0" + tens;
+        }
+
+        if(tens > 9){
+            OutputTens.innerHTML = tens;
+        }
+
+        if(tens > 99){
+            seconds++;
+            OutputSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            OutputTens.innerHTML = "0" + 0;
+        }
+
+        if(seconds > 9){
+            OutputSeconds.innerHTML = seconds;
+        }
     }
 }
-
-buttonStart.onclick = function () {
-    interval = setInterval(startTimer);
-};
-
-buttonStop.onclick = function () {
-    clearInterval(interval);
-};
-
-buttonReset.onclick = function () {
-    clearInterval(interval);
-    tens = "00";
-    seconds = "00";
-    appendSeconds.innerHTML = seconds;
-    appendTens.innerHTML = tens;
-};
