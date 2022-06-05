28a7a421b8c8bd26b46f508d621e4daf9654cf2a
JonEnrique <100391192+JonEnrique@users.noreply.github.com>
2022-06-05 10:22:38 +1000

    hi

--- public/stopwatch.js
+++ public/stopwatch.js
@@ -1,47 +1,49 @@
-var seconds = 00;
-var tens = 00;
-var OutputSeconds = document.getElementById("second");
-var OutputTens = document.getElementById("tens");
-var buttonStart = document.getElementById("button-start");
-var buttonStop = document.getElementById("button-stop");
-var buttonReset = document.getElementById("button-reset");
-var Interval;
+window.onload = function () {
+    var seconds = 00;
+    var tens = 00;
+    var OutputSeconds = document.getElementById("second");
+    var OutputTens = document.getElementById("tens");
+    var buttonStart = document.getElementById("button-start");
+    var buttonStop = document.getElementById("button-stop");
+    var buttonReset = document.getElementById("button-reset");
+    var Interval;
 
-buttonStart.addEventListener('click', () => {
-    clearInterval(Interval);
-    Interval = setInterval(startTimer, 10);  // millisecond 10 = 0.01 second
-});
+    buttonStart.addEventListener('click', () => {
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);  // millisecond 10 = 0.01 second
+    });
 
-buttonStop.addEventListener('click', () => {
-    clearInterval(Interval);
-});
+    buttonStop.addEventListener('click', () => {
+        clearInterval(Interval);
+    });
 
-buttonReset.addEventListener('click', () => {
-    clearInterval(Interval);
-    tens = "00";
-    seconds = "00";
-    OutputSeconds.innerHTML = seconds;
-    OutputTens.innerHTML = tens;
-});
-
-function startTimer() {
-    tens++;
-    if (tens <= 9) {
-        OutputTens.innerHTML = "0" + tens;
-    }
-
-    if (tens > 9) {
-        OutputTens.innerHTML = tens;
-    }
-
-    if (tens > 99) {
-        seconds++;
-        OutputSeconds.innerHTML = "0" + seconds;
-        tens = 0;
-        OutputTens.innerHTML = "0" + 0;
-    }
-
-    if (seconds > 9) {
+    buttonReset.addEventListener('click', () => {
+        clearInterval(Interval);
+        tens = "00";
+        seconds = "00";
         OutputSeconds.innerHTML = seconds;
+        OutputTens.innerHTML = tens;
+    });
+
+    function startTimer() {
+        tens++;
+        if (tens <= 9) {
+            OutputTens.innerHTML = "0" + tens;
+        }
+
+        if (tens > 9) {
+            OutputTens.innerHTML = tens;
+        }
+
+        if (tens > 99) {
+            seconds++;
+            OutputSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            OutputTens.innerHTML = "0" + 0;
+        }
+
+        if (seconds > 9) {
+            OutputSeconds.innerHTML = seconds;
+        }
     }
 }
