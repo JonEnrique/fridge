ede75b404ebfb158ac68f4c23b559364897bf5f4
JonEnrique <100391192+JonEnrique@users.noreply.github.com>
2022-06-05 14:34:32 +1000

    music

---
+++ public/musicplayer.js
@@ -0,0 +1,20 @@
+var a=0.2;
+function start() {
+  document.getElementById('song').volume=a;
+  document.getElementById('song').play();
+}
+function stop() {
+  document.getElementById('song').pause();
+}
+function up() {
+  if(window.a>0.9)
+    window.a=0.9;
+  window.a+=0.1;
+  document.getElementById('song').volume=a;
+}
+function down() {
+  if(window.a<0.1)
+    window.a=0.1;
+  window.a-=0.1;
+  document.getElementById('song').volume=a;
+}
